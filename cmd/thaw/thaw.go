65d61c05b7f75a9185ca964a12f92954b5c16b3b
seantcanavan@gmail.com <seantcanavan@gmail.com>
2023-04-23 13:20:33 -0500

    use custom type since the golang sdk has pointers on one side and dynamodb stream sdk does not have it on theirs

--- cmd/thaw/thaw.go
+++ cmd/thaw/thaw.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/seantcanavan/notification-step-machine/database_job"
 	"github.com/seantcanavan/notification-step-machine/database_ttl"
+	"github.com/seantcanavan/notification-step-machine/job"
 	"time"
 )
 
@@ -18,12 +20,91 @@ func init() {
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(lambdaHandler)
 }
 
-func handler(_ context.Context, event events.DynamoDBEvent) error {
+// changed type of event from: events.DynamoDBEvent to DynamoDBEvent (see below)
+func lambdaHandler(event DynamoDBEvent) error {
+
 	for _, record := range event.Records {
-		fmt.Println(fmt.Sprintf("record is [%+v]", record))
+		var beforeCReq job.CreateReq
+		var afterCReq job.CreateReq
+
+		change := record.Change
+		newImage := change.NewImage // now of type: map[string]*dynamodb.AttributeValue
+		oldImage := change.OldImage // now of type: map[string]*dynamodb.AttributeValue
+
+		err1 := dynamodbattribute.UnmarshalMap(oldImage, &beforeCReq)
+		if err1 != nil {
+			return err1
+		}
+
+		err2 := dynamodbattribute.UnmarshalMap(newImage, &afterCReq)
+		if err2 != nil {
+			return err2
+		}
+
+		fmt.Println(fmt.Sprintf("beforeCReq [%+v]", beforeCReq))
+		fmt.Println(fmt.Sprintf("afterCReq [%+v]", afterCReq))
 	}
+
 	return nil
 }
+
+type IdOnly struct {
+	Id string `json:"id"`
+}
+
+type DynamoDBEvent struct {
+	Records []DynamoDBEventRecord `json:"Records"`
+}
+
+type DynamoDBEventRecord struct {
+	AWSRegion      string                       `json:"awsRegion"`
+	Change         DynamoDBStreamRecord         `json:"dynamodb"`
+	EventID        string                       `json:"eventID"`
+	EventName      string                       `json:"eventName"`
+	EventSource    string                       `json:"eventSource"`
+	EventVersion   string                       `json:"eventVersion"`
+	EventSourceArn string                       `json:"eventSourceARN"`
+	UserIdentity   *events.DynamoDBUserIdentity `json:"userIdentity,omitempty"`
+}
+
+type DynamoDBStreamRecord struct {
+	ApproximateCreationDateTime events.SecondsEpochTime `json:"ApproximateCreationDateTime,omitempty"`
+	// changed to map[string]*dynamodb.AttributeValue
+	Keys map[string]*dynamodb.AttributeValue `json:"Keys,omitempty"`
+	// changed to map[string]*dynamodb.AttributeValue
+	NewImage map[string]*dynamodb.AttributeValue `json:"NewImage,omitempty"`
+	// changed to map[string]*dynamodb.AttributeValue
+	OldImage       map[string]*dynamodb.AttributeValue `json:"OldImage,omitempty"`
+	SequenceNumber string                              `json:"SequenceNumber"`
+	SizeBytes      int64                               `json:"SizeBytes"`
+	StreamViewType string                              `json:"StreamViewType"`
+}
+
+//func main() {
+//	lambda.Start(handler)
+//}
+//
+//func handler(_ context.Context, event events.DynamoDBEvent) error {
+//	for _, record := range event.Records {
+//		fmt.Println(fmt.Sprintf("record is [%+v]", record))
+//		var beforeCReq job.CreateReq
+//		var afterCReq job.CreateReq
+//		oldErr := dynamodbattribute.UnmarshalMap(record.Change.OldImage, &beforeCReq)
+//		if oldErr != nil {
+//			fmt.Println(fmt.Sprintf("oldErr [%+v]", oldErr))
+//		}
+//
+//		newErr := dynamodbattribute.UnmarshalMap(record.Change.NewImage, &afterCReq)
+//		if newErr != nil {
+//			fmt.Println(fmt.Sprintf("newErr [%+v]", newErr))
+//		}
+//
+//		fmt.Println(fmt.Sprintf("beforeCReq [%+v]", beforeCReq))
+//		fmt.Println(fmt.Sprintf("afterCReq [%+v]", afterCReq))
+//
+//	}
+//	return nil
+//}
