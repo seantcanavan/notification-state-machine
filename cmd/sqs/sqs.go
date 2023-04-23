8e63c2de385ddc69d4572db54e6b81cfae79d1af
seantcanavan@gmail.com <seantcanavan@gmail.com>
2023-04-23 10:56:49 -0500

    finish sqs handler

--- cmd/sqs/sqs.go
+++ cmd/sqs/sqs.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/seantcanavan/notification-step-machine/database_job"
 	"github.com/seantcanavan/notification-step-machine/database_ttl"
+	"github.com/seantcanavan/notification-step-machine/job"
 	"time"
 )
 
@@ -14,5 +20,23 @@ func init() {
 }
 
 func main() {
+	lambda.Start(handler)
+}
 
+func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, record := range sqsEvent.Records {
+		var cReq job.CreateReq
+		err := json.Unmarshal([]byte(record.Body), &cReq)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("error unmarshalling record [%+v]", record))
+			continue
+		}
+
+		_, httpStatus, err := job.Create(ctx, &cReq)
+		if err != nil {
+			return fmt.Errorf("code [%d] error [%+v] creating job [%+v]", httpStatus, err, cReq)
+		}
+	}
+
+	return nil
 }
