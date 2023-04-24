dccbbdcb346e4020bdafc4fe16595d48f05bd26b
seantcanavan@gmail.com <seantcanavan@gmail.com>
2023-04-23 21:42:54 -0500

    finish nudge code

--- cmd/cleanup/cleanup.go
+++ cmd/cleanup/cleanup.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/seantcanavan/error_group"
 	"github.com/seantcanavan/notification-step-machine/database_job"
 	"github.com/seantcanavan/notification-step-machine/database_ttl"
 	"github.com/seantcanavan/notification-step-machine/enum"
+	"github.com/seantcanavan/notification-step-machine/job"
 	"net/http"
 	"time"
 )
@@ -31,16 +31,12 @@ func Handler(ctx context.Context, input ServerlessInput) (int, error) {
 	if input.Type == "" {
 		return http.StatusBadRequest, fmt.Errorf("empty Type [%+v]", input.Type)
 	} else if input.Type == enum.Email {
-
+		return job.NudgeEmail(ctx)
 	} else if input.Type == enum.SMS {
-
+		return job.NudgeSMS(ctx)
 	} else if input.Type == enum.Snail {
-
+		return job.NudgeSnail(ctx)
 	} else {
 		return http.StatusBadRequest, fmt.Errorf("invalid Type [%+v]", input.Type)
 	}
-
-	esg := error_group.NewErrorStatusGroup()
-
-	return esg.ToStatusAndError()
 }
